e23fddcd1b86e710adcc1d2017a0519e3dbf9b93
kayleen <8iscogb9@anonaddy.me>
2022-03-01 15:15:08 -0600

    all operators working

--- calc.js
+++ calc.js
@@ -7,12 +7,41 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? (e.g. '+ 1 5') \n\n", function(input){
 	tokens = input.split(' ');
 	
-	mathSymbol = tokens[0];
+	mathOperator = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	
+
+	console.log('\n','mathOperator: ', mathOperator);
+	if (isNaN(num1) == false){
+		console.log('num1: ', num1);
+	}
+	if (isNaN(num2) == false){
+		console.log('num2: ', num2);
+	}
+
+	if (mathOperator === "+"){
+		console.log('the sum is: ', num1 + num2, '\n');
+	}
+	if (mathOperator === "-"){
+		console.log('the difference is: ', num1 - num2, '\n');
+	}
+	if (mathOperator === "*"){
+		console.log('the product is: ', num1 * num2, '\n');
+	}
+	if (mathOperator === "/"){
+		console.log('the quotient is: ', num1 / num2, '\n');
+	}
+	if (mathOperator === "sqrt"){
+		a = Math.sqrt(num1)
+		console.log('square root is: ', a, '\n');
+	}
+	if (mathOperator === "%"){
+		console.log('the remainder is: ', num1 % num2, '\n');
+	}
 
 
 	// This line closes the connection to the command line interface.
